6f490823269f8664691d63ea96bbba3fcceb2467
Thijs Daniels <thijsdaniels@codedazur.com>
2023-01-01 12:58:07 +0100

    feat: add word element

--- src/game.ts
+++ src/game.ts
@@ -1,31 +1,32 @@
-import { LitElement, css, html } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { LitElement, css, html } from "lit";
+import { customElement } from "lit/decorators.js";
 
-@customElement('hangman-game')
+@customElement("hangman-game")
 export class HangmanGame extends LitElement {
-  render() {
-    return html`
-      <hangman-input @value-changed="${this.valueChanged}"></hangman-input>
-      <hangman-button @click=${() => alert('click')}>Guess</hangman-button>
-    `
-  }
+	render() {
+		return html`
+			<hangman-word word="hello" .letters=${["e", "o"]}></hangman-word>
+			<hangman-input @value-changed="${this.valueChanged}"></hangman-input>
+			<hangman-button @click=${() => alert("click")}>Guess</hangman-button>
+		`;
+	}
 
-  valueChanged(e: CustomEvent) {
-    console.log(e.detail)
-  }
+	valueChanged(e: CustomEvent) {
+		console.log(e.detail);
+	}
 
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-  `
+	static styles = css`
+		:host {
+			max-width: 1280px;
+			margin: 0 auto;
+			padding: 2rem;
+			text-align: center;
+		}
+	`;
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'hangman-game': HangmanGame
-  }
+	interface HTMLElementTagNameMap {
+		"hangman-game": HangmanGame;
+	}
 }
