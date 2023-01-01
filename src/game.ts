a19c8889a42873ed12655c72c6a36968687c60f2
BramBekkers <bram.bekkers@outlook.com>
2023-01-01 13:57:30 +0100

    Add array logic to guess letters

--- src/game.ts
+++ src/game.ts
@@ -1,39 +1,40 @@
-
-import { LitElement, css, html } from 'lit'
+import { css, html, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 import { randomWord } from './utilities/words'
 
-
-@customElement("hangman-game")
+@customElement('hangman-game')
 export class HangmanGame extends LitElement {
-
-  @property()
-  word: string = randomWord()
+  @property() word: string = randomWord()
+  @property() guessedLetters: string[] = []
 
   render() {
     return html`
-      <hangman-word word="${this.word}" .letters=${["e", "o"]}></hangman-word>
+      <p>${this.guessedLetters}</p>
+      <hangman-word
+        word="${this.word}"
+        .letters=${this.guessedLetters}
+      ></hangman-word>
       <hangman-form @guess="${this.valueChanged}" />
     `
   }
 
   private valueChanged(e: CustomEvent) {
-    console.log(e.detail)
+    this.guessedLetters.push(e.detail)
+    this.guessedLetters = [...new Set(this.guessedLetters)]
   }
 
-
-	static styles = css`
-		:host {
-			max-width: 1280px;
-			margin: 0 auto;
-			padding: 2rem;
-			text-align: center;
-		}
-	`;
+  static styles = css`
+    :host {
+      max-width: 1280px;
+      margin: 0 auto;
+      padding: 2rem;
+      text-align: center;
+    }
+  `
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		"hangman-game": HangmanGame;
-	}
+  interface HTMLElementTagNameMap {
+    'hangman-game': HangmanGame
+  }
 }
